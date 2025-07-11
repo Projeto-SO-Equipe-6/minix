f9c72140e7cb63c52517f6509f188c3d2e713a9e
Rafael Poddis Busquim e Silva <46289953+RafaelPoddis@users.noreply.github.com>
2025-07-11 18:24:25 -0300

    Update proc.c

--- minix/kernel/proc.c
+++ minix/kernel/proc.c
@@ -348,6 +348,10 @@ static void sjf_insert_sorted(struct proc *p)
 {
 	int q = p->p_priority;
 	struct proc *curr, *prev = NULL;
+
+	struct proc **rdy_head, **rdy_tail;
+	rdy_head = get_cpu_var(p->p_cpu, run_q_head);
+	rdy_tail = get_cpu_var(p->p_cpu, run_q_tail);
 	
 	sjf_anti_starvation(p);
 
@@ -1685,8 +1689,11 @@ void enqueue(
  * This function can be used x-cpu as it always uses the queues of the cpu the
  * process is assigned to.
  */
-  int q = rp->p_priority;	 		/* scheduling queue to use */
+  int q = rp->p_priority;
+  struct proc **rdy_head, **rdy_tail;
 
+  rdy_head = get_cpu_var(rp->p_cpu, run_q_head);
+  rdy_tail = get_cpu_var(rp->p_cpu, run_q_tail);
 	if (q >= USER_Q && q <= MIN_USER_Q) {
 		rp->p_wait_start_time = get_monotonic();
 		sjf_insert_sorted(rp);
@@ -1699,60 +1706,6 @@ void enqueue(
 		}
 		rp->p_nextready = NULL;
 	}
-}
-/*
-  struct proc **rdy_head, **rdy_tail;
-  
-  assert(proc_is_runnable(rp));
-
-  assert(q >= 0);
-
-  rdy_head = get_cpu_var(rp->p_cpu, run_q_head);
-  rdy_tail = get_cpu_var(rp->p_cpu, run_q_tail);
-
-  /* Now add the process to the queue. */
-  if (!rdy_head[q]) {		/* add to empty queue */
-      rdy_head[q] = rdy_tail[q] = rp; 		/* create a new queue */
-      rp->p_nextready = NULL;		/* mark new end */
-  } 
-  else {					/* add to tail of queue */
-      rdy_tail[q]->p_nextready = rp;		/* chain tail of queue */	
-      rdy_tail[q] = rp;				/* set new queue tail */
-      rp->p_nextready = NULL;		/* mark new end */
-  }
-
-  if (cpuid == rp->p_cpu) {
-	  /*
-	   * enqueueing a process with a higher priority than the current one,
-	   * it gets preempted. The current process must be preemptible. Testing
-	   * the priority also makes sure that a process does not preempt itself
-	   */
-	  struct proc * p;
-	  p = get_cpulocal_var(proc_ptr);
-	  assert(p);
-	  if((p->p_priority > rp->p_priority) &&
-			  (priv(p)->s_flags & PREEMPTIBLE))
-		  RTS_SET(p, RTS_PREEMPTED); /* calls dequeue() */
-  }
-#ifdef CONFIG_SMP
-  /*
-   * if the process was enqueued on a different cpu and the cpu is idle, i.e.
-   * the time is off, we need to wake up that cpu and let it schedule this new
-   * process
-   */
-  else if (get_cpu_var(rp->p_cpu, cpu_is_idle)) {
-	  smp_schedule(rp->p_cpu);
-  }
-#endif
-
-  /* Make note of when this process was added to queue */
-  read_tsc_64(&(get_cpulocal_var(proc_ptr)->p_accounting.enter_queue));
-
-
-#if DEBUG_SANITYCHECKS
-  assert(runqueues_ok_local());
-#endif
-*/
 }
 
 /*===========================================================================*
